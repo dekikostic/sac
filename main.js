b617014cd8105e373389681c9d7cae8acd2fc57d
dekikostic <83778040+dekikostic@users.noreply.github.com>
2021-05-11 12:34:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ var getPromisify = (url, data, dataType) => {
       console.log("Get executed")
       const r = await getPromisify(url, data, dataType)
       return [
-        r.json().response.positiveCasesViral,
-        r.json().response.positiveIncrease,
-        r.json().response.hospitalizedCurrently
+        r.response.json().positiveCasesViral,
+        r.response.json().positiveIncrease,
+        r.response.json().hospitalizedCurrently
       ]
     }
   }
