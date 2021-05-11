806c8cd38037ccc39ef14eea6ab4844563a70947
Kostic <dejan.kostic@sap.com>
2021-05-11 10:11:02 +0200

    c

--- people_main.js
+++ people_main.js
@@ -1,32 +1,35 @@
 
 var url = "https://ghibliapi.herokuapp.com/people"
 
-function fetchedData(url){
+function fetchData(url){
   const response = await fetch(url)
   return response.json()
 }
-
+    
 (function () {
-  // const template = document.createElement('template')
-  // template.innerHTML = `
-  //     <style>
-  //     </style>
-  //     <div id="root" style="width: 100%; height: 100%;">
-  //     </div>
-  //   `
+  const template = document.createElement('template')
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `
   class MainWebComponent extends HTMLElement {
     // ------------------
     // Scripting methods
     // ------------------
+     
     async get(index) {
-      fetchedData(url).then( data => {
+      fetchData(url).then( data => {
         return [
           data[index].name,
           data[index].age,
           data[index].gender
         ]
-      }
+      })
     }
-    customElements.define('dk-test-people', MainWebComponent)
   }
-})()
+
+  customElements.define('dk-test-covid2', MainWebComponent)
+}
+)()
