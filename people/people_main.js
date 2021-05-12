02bc5173be066842c5bfcff28b346a0ce3d39391
Kostic <dejan.kostic@sap.com>
2021-05-12 13:25:06 +0200

    name changed

--- people/people_main.js
+++ people/people_main.js
@@ -1,35 +1,44 @@
+var getPromisify = (url, data, dataType) => {
+  return new Promise((resolve, reject) => {
+    $.get(
+      url,
+      data,
+      (response, status, xhr) => {
+        if (status === "success") {
+          resolve({ response, status, xhr });
+        } else {
+          const err = new Error("xhr error");
+          err.target = xhr;
+          reject(err);
+          console.log("xhr error");
+        }
+      },
+      dataType
+    );
+  });
+};
 
-var url = "https://ghibliapi.herokuapp.com/people"
-
-function fetchData(url){
-  const response = await fetch(url)
-  return response.json()
-}
-    
 (function () {
-  const template = document.createElement('template')
+  const template = document.createElement("template");
   template.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
-    `
+    `;
   class MainWebComponent extends HTMLElement {
     // ------------------
     // Scripting methods
     // ------------------
-     
-    async get(index) {
-      fetchData(url).then( data => {
-        return [
-          data[index].name,
-          data[index].age,
-          data[index].gender
-        ]
-      })
+    async get(url, data, dataType) {
+      const r = await getPromisify(url, data, dataType);
+      return [
+        r.response[data].name,
+        r.response[data].age,
+        r.response[data].gender,
+      ];
     }
   }
 
-  customElements.define('dk-test-people', MainWebComponent)
-}
-)()
+  customElements.define("dk-test-people", MainWebComponent);
+})();
