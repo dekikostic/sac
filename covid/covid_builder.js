d67ed21bb97c158a6afe0face6333c11b5120a08
dekikostic <dekikostic@gmail.com>
2021-05-12 14:27:34 +0200

    s

--- covid/covid_builder.js
+++ covid/covid_builder.js
@@ -1,6 +1,6 @@
-(function()  {
-	let template = document.createElement("template");
-	template.innerHTML = `
+(function () {
+  let template = document.createElement("template");
+  template.innerHTML = `
 		<form id="form">
 			<fieldset>
 				<legend>Run Parameters</legend>
@@ -33,33 +33,37 @@
 		</style>
 	`;
 
-	class ColoredBoxBuilderPanel extends HTMLElement {
-		constructor() {
-			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-		}
+  class ColoredBoxBuilderPanel extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this._shadowRoot
+        .getElementById("form")
+        .addEventListener("submit", this._submit.bind(this));
+    }
 
-		_submit(e) {
-			e.preventDefault();
-			this.dispatchEvent(new CustomEvent("propertiesChanged", {
-					detail: {
-						properties: {
-							opacity: this.opacity
-						}
-					}
-			}));
-		}
+    _submit(e) {
+      e.preventDefault();
+      this.dispatchEvent(
+        new CustomEvent("propertiesChanged", {
+          detail: {
+            properties: {
+              opacity: this.opacity,
+            },
+          },
+        })
+      );
+    }
 
-		set opacity(newOpacity) {
-			this._shadowRoot.getElementById("builder_opacity").value = newOpacity;
-		}
+    set opacity(newOpacity) {
+      this._shadowRoot.getElementById("builder_opacity").value = newOpacity;
+    }
 
-		get opacity() {
-			return this._shadowRoot.getElementById("builder_opacity").value;
-		}
-	}
+    get opacity() {
+      return this._shadowRoot.getElementById("builder_opacity").value;
+    }
+  }
 
-	customElements.define("dk-test-covid2-builder", ColoredBoxBuilderPanel);
+  customElements.define("dk-test-covid-builder", ColoredBoxBuilderPanel);
 })();
