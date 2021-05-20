505fe7b50c070ac46aa35c8346a0248b273b7257
dekikostic <dekikostic@gmail.com>
2021-05-20 16:13:50 +0200

    papm run func

---
+++ papm_run_func/builder.js
@@ -0,0 +1,69 @@
+(function () {
+  let template = document.createElement("template");
+  template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Run Parameters</legend>
+				<table>
+					<tr>
+						<td>Environment</td>
+						<td><input id="builder_env_id" type="text" size="10" maxlength="3"></td>
+					</tr>
+					<tr>
+						<td>Version</td>
+						<td><input id="builder_ver" type="text" size="10" maxlength="4"></td>
+					</tr>
+					<tr>
+						<td>Function ID</td>
+						<td><input id="builder_fid" type="text" size="10" maxlength="5"></td>
+					</tr>
+					<tr>
+						<td>Process ID</td>
+						<td><input id="builder_proc_id" type="text" size="10" maxlength="7"></td>
+					</tr>
+				</table>
+				<input type="submit" style="display:none;">
+			</fieldset>
+		</form>
+		<style>
+		:host {
+			display: block;
+			padding: 1em 1em 1em 1em;
+		}
+		</style>
+	`;
+
+  class RunFunctionBuilderPanel extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this._shadowRoot
+        .getElementById("form")
+        .addEventListener("submit", this._submit.bind(this));
+    }
+
+    _submit(e) {
+      e.preventDefault();
+      this.dispatchEvent(
+        new CustomEvent("propertiesChanged", {
+          detail: {
+            properties: {
+              env_id: this.env_id,
+            },
+          },
+        })
+      );
+    }
+
+    set env_id(env_id) {
+      this._shadowRoot.getElementById("builder_env_id").value = env_id;
+    }
+
+    get env_id() {
+      return this._shadowRoot.getElementById("builder_env_id").value;
+    }
+  }
+
+  customElements.define("papm-run-fun", RunFunctionBuilderPanel);
+})();
---
+++ papm_run_func/main.js
@@ -0,0 +1,46 @@
+var getPromisify = (url, data, dataType) => {
+  return new Promise((resolve, reject) => {
+    $.post(
+      url,
+      data,
+      (response, status, xhr) => {
+        if (status === "success") {
+          resolve({ response, status, xhr });
+        } else {
+          const err = new Error("xhr error");
+          err.target = xhr;
+          reject(err);
+          console.log("xhr error");
+        }
+      },
+      dataType
+    );
+  });
+};
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    // ------------------
+    // Scripting methods
+    // ------------------
+    async get(url, data, dataType) {
+      console.log(this._props);
+      const r = await getPromisify(url, data, dataType);
+      console.log(r);
+      // return [
+      //   r.response[data].name,
+      //   r.response[data].age,
+      //   r.response[data].gender,
+      // ];
+    }
+  }
+
+  customElements.define("papm-run-func", MainWebComponent);
+})();
