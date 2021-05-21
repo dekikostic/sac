97e546c197910f0c55eb4bfdfc846bae3b510b3f
dekikostic <dekikostic@gmail.com>
2021-05-21 17:31:53 +0200

    ;

--- papm_run_func/main.js
+++ papm_run_func/main.js
@@ -31,7 +31,7 @@ var getPromisify = (url, data, dataType) => {
     // Scripting methods
     // ------------------
     async run(url, data, dataType) {
-      console.log(`Properties: ${this}`);
+      console.log(`This: ${this}`);
       // console.log(`Env id: ${this._props[env_id]}`);
       // const r = await getPromisify(url, data, dataType);
       // console.log(r);
