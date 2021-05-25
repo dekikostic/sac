33e8fe4f2dc9bc1a8c8690891b138ccbddc0f531
dekikostic <dekikostic@gmail.com>
2021-05-25 22:35:08 +0200

    p

--- papm_run_func/main.js
+++ papm_run_func/main.js
@@ -37,15 +37,7 @@ var getPromisify = (url, data, dataType) => {
       this._props = { ...this._props, ...changedProperties };
     }
 
-    onCustomWidgetAfterUpdate(changedProperties) {
-      if ("env_id" in changedProperties) {
-        var env_id = changedProperties["env_id"];
-        console.log(env_id);
-      }
-      if ("ver" in changedProperties) {
-        console.log(changedProperties["ver"]);
-      }
-    }
+    onCustomWidgetAfterUpdate(changedProperties) {}
 
     // ------------------
     // Scripting methods
