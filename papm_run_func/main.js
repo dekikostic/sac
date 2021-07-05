4813f880ca2f6a29a21350eb5ea6895fc432cf93
dekikostic <dekikostic@gmail.com>
2021-07-05 12:47:49 +0200

    const test

--- papm_run_func/main.js
+++ papm_run_func/main.js
@@ -30,11 +30,12 @@
 
       // const runParams = `/RunAsync?EnvId='${this._props.env_id}'&Ver='${this._props.ver}'&ProcId=''&Activity=''&Fid='${this._props.fid}'`;
 
+      let env_id = this._props.env_id;
       const raw = JSON.stringify({
-        envId: this._props.env_id,
+        envId: env_id,
         ver: this._props.ver,
         procId: this._props.procId,
-        activity: "",
+        activity: "TEST",
         fid: this._props.fid,
       });
 
