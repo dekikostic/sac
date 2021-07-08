9ccc6c822b00c1cc089a9c600f2c8cff7ed3fecd
dekikostic <dekikostic@gmail.com>
2021-07-08 16:57:49 +0200

    response.text()

--- papm_run_func/main.js
+++ papm_run_func/main.js
@@ -26,36 +26,36 @@
       const BASE_URL = "http://localhost:3500/runAsync";
       const papmUrl = url != "" ? url : BASE_URL;
 
-      try {
-        const runRequest = await fetch(
-          `${papmUrl}?EnvId='${this._props.env_id}'&Ver='${this._props.ver}'&ProcId=''&Activity=''&Fid='${this._props.fid}'`,
-          {
-            method: "POST",
-            mode: "no-cors",
-          }
-        );
+      // try {
+      //   const runRequest = await fetch(
+      //     `${papmUrl}?EnvId='${this._props.env_id}'&Ver='${this._props.ver}'&ProcId=''&Activity=''&Fid='${this._props.fid}'`,
+      //     {
+      //       method: "POST",
+      //       mode: "no-cors",
+      //     }
+      //   );
 
-        const runResponse = await runRequest.json();
+      //   const runResponse = await runRequest.json();
 
-        console.log(runResponse);
+      //   console.log(runResponse);
 
-        return runResponse;
+      //   return runResponse;
 
-        // return msg.value;
-      } catch (status) {
-        console.log(status);
-        new Error();
-      }
-      // fetch(
-      //   "http://localhost:3500/runAsync?EnvId='SXP'&Ver='0007'&ProcId=''&Activity=''&Fid='CAACD'",
-      //   {
-      //     method: "POST",
-      //     mode: "no-cors",
-      //   }
-      // )
-      //   .then((response) => response.text())
-      //   .then((result) => console.log(result))
-      //   .catch((error) => console.log("error", error));
+      //   // return msg.value;
+      // } catch (status) {
+      //   console.log(status);
+      //   new Error();
+      // }
+      fetch(
+        `${papmUrl}?EnvId='${this._props.env_id}'&Ver='${this._props.ver}'&ProcId=''&Activity=''&Fid='${this._props.fid}'`,
+        {
+          method: "POST",
+          mode: "no-cors",
+        }
+      )
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
     }
   }
 
